83f3dd5f0d3ff66acda7d412189fa768363d63a3
Kolton Hahn <kolthahn97@gmail.com>
2022-08-26 14:40:45 -0600

    Simple Navigation Element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import { firebaseAuth } from 'firebase-config'
 import { onAuthStateChanged } from 'firebase/auth';
+import Header from 'components/header/Header';
 
 export default function App({user}) {
   console.log(user)
   return (
     <div className='App'>
+      <Header />
       {user.displayName ? 'HELLO ' + user.displayName : 'Who are you?'}
     </div>
   );
