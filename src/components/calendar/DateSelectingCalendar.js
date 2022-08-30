0b9463caa82381bf3585a469595163de9909801e
Kolton Hahn <kolthahn97@gmail.com>
2022-08-30 14:21:31 -0600

    Added a datepicker
    Added a calendar

--- src/components/calendar/DateSelectingCalendar.js
+++ src/components/calendar/DateSelectingCalendar.js
@@ -1,6 +1,22 @@
+import { TextField } from "@mui/material";
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import moment from "moment";
+import { useState } from "react";
+
 export default function DateSelectingCalendar() {
-  
+  const [value, setValue] = useState(moment())
+
   return (
-    <div>calendar</div>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <StaticDatePicker 
+        displayStaticWrapperAs="desktop"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
   );
 }
