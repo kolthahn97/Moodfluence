4475bebd00d5507b8bd9d81296dea1fc1b59d95f
Kolton Hahn <kolthahn97@gmail.com>
2022-08-30 17:05:16 -0600

    Added Tabs to profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,8 +1,63 @@
-import Header from 'components/header/Header'
+import { Grid, Tab, Tabs, Typography } from '@mui/material'
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import styles from './Profile.module.scss'
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 export default function Profile() {
-    return <div className='profile'>
-        
-    </div>
+    const [ tabValue, setTabValue ] = useState(0)
+    
+    const handleChange = (event, newValue) => {
+        setTabValue(newValue);
+    };
+
+    return <Grid 
+        container 
+        className='profile'
+        justifyContent='center'
+        alignItems='center'
+    >
+        <Grid item xs={12} md={9} lg={6}>
+            <Tabs value={tabValue} onChange={handleChange} aria-label='Profile Tabs'>
+                <Tab label='Item One' {...a11yProps(0)} />
+                <Tab label='Item Two' {...a11yProps(1)} />
+                <Tab label='Item Three' {...a11yProps(2)} />
+            </Tabs>
+            <TabPanel value={tabValue} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={tabValue} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={tabValue} index={2}>
+                Item Three
+            </TabPanel>
+        </Grid>
+    </Grid>
 }
